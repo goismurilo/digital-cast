64c5ec01cbae81d7db883f996d956f784e69f47d
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-12-13 22:25:53 -0300

    refactor(Landing): Change Landing Page

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from 'react-native';
-
 
 import { Background } from './styles';
 
---
+++ src/pages/Player/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Player: React.FC = () => {
+    return (
+        <SafeAreaView>
+            <Text></Text>
+        </SafeAreaView>
+    )
+}
+
+export default Player;
