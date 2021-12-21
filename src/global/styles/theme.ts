a9b5ea204c6518bc284f22ede3c97ed19b453c9e
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-12-20 22:33:15 -0300

    feat(Theme): Add files Global Themes

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,11 @@
+export default {
+    colors: {
+        text: '#FFF',
+    },
+
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',
+    }
+}
